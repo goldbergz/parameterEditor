f461b4bfc278ac46baae517e2a6076a10db76d6f
goldbergz <zolotogornaya22@mail.ru>
2026-01-28 12:42:06 +0300

    feat: add project launch

--- param-editor/src/App.tsx
+++ param-editor/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { ParamEditor, ParamEditor as ParamEditorType } from "./ParamEditor";
+import type { Param, Model } from "./ParamEditor";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const params: Param[] = [
+  { id: 1, name: "Назначение", type: "string" },
+  { id: 2, name: "Длина", type: "string" },
+];
+
+const model: Model = {
+  paramValues: [
+    { paramId: 1, value: "повседневное" },
+    { paramId: 2, value: "макси" },
+  ],
+  colors: [],
+};
+
+export default function App() {
+  const ref = useRef<ParamEditorType>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: 20 }}>
+      <h2>Редактор параметров</h2>
+      <ParamEditor ref={ref} params={params} model={model} />
 
-export default App
+      <button
+        onClick={() => {
+          console.log(ref.current?.getModel());
+          alert("Model в консоли");
+        }}
+      >
+        Получить Model
+      </button>
+    </div>
+  );
+}
